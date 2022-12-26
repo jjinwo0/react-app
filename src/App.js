62b688d30207ea9196667848a67e2fc01a390659
Jinwoo Park <zvzv9808@gmail.com>
2022-12-26 21:30:28 +0900

    commit with useEffect() function

--- src/App.js
+++ src/App.js
@@ -1,11 +1,37 @@
 import Button from "./Button";
 import styles from "./App.module.css";
+import { useState, useEffect } from "react";
+import React from "react";
 
 function App() {
+  const [counter, setValue] = useState(0);
+  const [keyword, setKeyword] = useState("");
+
+  const onClick = () => setValue((prev) => prev + 1);
+  const onChange = (event) => setKeyword(event.target.value);
+  
+  //useEffect() -> 코드가 단 한번 실행할 수 있도록 보호
+  useEffect(() => {
+    console.log("I run only once.");
+  }, []);
+  useEffect(() => {
+    console.log("I run when 'keyword' changes.");
+  }, [keyword]);
+  useEffect(() => {
+    console.log("I run when 'counter' changes.");
+  }, [counter]);
+  useEffect(() => {
+    console.log("I run when keyword & counter changes.");
+  }, [keyword, counter]);
   return (
-    <div>
-      <h1 className={styles.title}>Welcome back!!!</h1>
-      <Button text={"Continue"} />
+    <div> 
+      <input
+        value={keyword}
+        type="text" 
+        placeholder="Search here..." 
+        onChange={onChange}/>
+      <h1>{counter}</h1>
+      <button onClick={onClick}>click me</button>
     </div>
   );
 }
