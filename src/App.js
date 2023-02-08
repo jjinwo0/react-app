01d33fdf83ed553dd5d9b606c25ce109b02d66ae
jinwoo <zvzv9808@gmail.com>
2023-02-08 19:33:54 +0900

    CoinTracker

--- src/App.js
+++ src/App.js
@@ -4,36 +4,30 @@ import { useState, useEffect } from "react";
 import React from "react";
 
 function App() {
-  const [toDo, setToDo] = useState("");
-  const [toDos, setToDos] = useState([]);
-  const onChange = (event) => setToDo(event.target.value);
-  const onSubmit = (event) => {
-    event.preventDefault(); //submit 동작 이후 페이지가 새로 고침 되는 것을 막아줌. (동작 결과 유지)
-    if(toDo === ""){
-      return ;
-    }
-    setToDos(currentArray => [toDo, ...currentArray]);
-    setToDo("");
-  }
-  console.log(toDos);
-  return (
+  const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([]);
+  useEffect(() => {
+    fetch("https://api.coinpaprika.com/v1/tickers")
+    .then((response) => response.json())
+    .then((json) => {
+      setCoins(json);
+      setLoading(false);
+      console.log(coins);
+    });
+  }, [])
+  return(
     <div>
-      <h1>My To Dos ({toDos.length})</h1>
-      <form onSubmit={onSubmit}>
-        <input
-          onChange={onChange} 
-          value={toDo} 
-          type="text" 
-          placeholder="Write your to do..."
-        />
-        <button>Add To Do</button>
-      </form>
-      <hr />
-      <ul>
-        {toDos.map((item, index) => 
-          <li key={index}>{item}</li>
-        )}
-      </ul>
+      <h1>The Coins! {loading ? "" : `(${coins.length})`}</h1>
+      {loading ? <strong>Loading...</strong> : 
+        <select>
+          {coins.map((coin) => (
+          <option id="op">{coin.name} ({coin.symbol}) : {coin.quotes.USD.price} USD</option>
+          ))}
+        </select>
+      }
+      <div>
+        {open.name}
+      </div>
     </div>
   );
 }
